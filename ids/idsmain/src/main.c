392bd887e67b06b85890f22bdadd1a20a506691d
Marianne Fletcher <mfletche@astral.cms.waikato.ac.nz>
2018-11-01 08:46:34 +1300

    Add function to import IP addresses from Firehol blacklist.

--- ids/idsmain/src/main.c
+++ ids/idsmain/src/main.c
@@ -10,12 +10,16 @@
 #include <stdio.h>
 #include <ctype.h>
 
+#include <getopt.h>
+
 #include "iface.h"
 #include "linked_list.h"
 #include "io_task.h"
 #include "mdns_io_task.h"
 #include "pcap_io_task.h"
 #include "ip_blacklist.h"
+#include "firehol_ip_blacklist.h"
+#include "ids_event_queue.h"
 
 /* -- DEBUGGING -- */
 #define DEBUG 1
@@ -23,7 +27,13 @@
 
 /* -- COMMAND LINE ARGUMENTS -- */
 const static char *getopt_args = "hp:i:";
+const static struct option long_options[] = {
+		{"ipbl", 1, 0, 0 },
+};
 const static char *getopt_usage = "\nRun as: %s -p port -i device1 -i device2 -i devicen.\n\n";
+
+const static int MAX_EVENTS = 5;
+const static int MAX_TS = 5;
 
 /* A list of interface names taken from the command line */
 struct linked_list *iface_list = NULL;
@@ -35,7 +45,11 @@ static int server_port = -1;
 struct io_task *tasks = NULL;
 
 /* Blacklist */
+char *ip_bl_file = NULL;
 ip_blacklist *ip_bl = NULL;
+
+/* Event queue */
+struct ids_event_list *ids_events = NULL;
 
 /*
  * Free all the global variables prior to exiting the program.
@@ -50,21 +64,37 @@ on_exit_callback()
 	free_linked_list(&iface_list, NULL);
 	free_io_task(&tasks);
 	free_ip_blacklist(&ip_bl);
+	free_ids_event_list(&ids_events);
 }
 
 int
 parse_args(int argc, char **argv)
 {
 	char *program = NULL;
-	int option_char, iface_num = 0, success = 1;
+	int option_char, iface_num = 0, success = 1, option_index = 0;
 
 	if (argc < 1) return (0);
 
 	program = argv[0];
 
-	while (-1 != (option_char = getopt (argc, argv, getopt_args))) {
+	while (-1 != (option_char = getopt_long (argc, argv, getopt_args, long_options, &option_index))) {
 		switch (option_char)
 		{
+			case 0:
+				/* Is a long option */
+
+				/* Only one long option: ipbl */
+				if (!optarg)
+				{
+					DPRINT("Did not receive an option for option %s\n", long_options[option_index].name);
+					break;
+				}
+				else
+				{
+					DPRINT("Argument --ipbl (IP blacklist file): %s\n", optarg);
+					ip_bl_file = optarg;
+				}
+				break;
 			case 'h':
 				fprintf(stderr, getopt_usage, program);
 				return (1);
@@ -144,18 +174,61 @@ parse_args(int argc, char **argv)
 int
 setup_blacklists()
 {
+	struct ip4_address_range *firehol_list = NULL;
+	FILE *fp = NULL;
+
+	if (!(ids_events = new_ids_event_list(MAX_EVENTS, MAX_TS)))
+	{
+		DPRINT("setup_blacklists(): new_ids_event_list() failed\n");
+		goto error;
+	}
+
 	ip_bl = new_ip_blacklist();
-	if (!ip_bl) return (0);
+	if (!ip_bl) goto error;
 
+	if (ip_bl_file)
+	{
+		fp = fopen(ip_bl_file, "r");
+		if (!fp) exit(EXIT_FAILURE);
 
+		firehol_list = read_firehol_ip_blacklist(fp);
+		fclose(fp);
+		fp = NULL;
+
+		struct ip4_address_range *ip4_iter = firehol_list;
+		while (ip4_iter)
+		{
+			/* Don't add really large address ranges */
+			if (ip4_iter->prefix_len >= 24)
+			{
+				uint32_t addr_iter;
+				uint32_t max_addr = ip4_address_range_get_max_addr(ip4_iter);
+
+				for (addr_iter = ip4_address_range_get_min_addr(ip4_iter); addr_iter < max_addr; addr_iter++)
+				{
+					if (!ip_blacklist_add(ip_bl, addr_iter)) goto error;
+				}
+
+				if (!ip_blacklist_add(ip_bl, max_addr)) goto error;
+			}
+
+			ip4_iter = ip4_iter->next;
+		}
+
+		free_ip4_address_range(&firehol_list);
+	}
 
 	return (1);
+error:
+	if (fp) fclose(fp);
+	free_ip4_address_range(&firehol_list);
+	return (0);
 }
 
 int
 main(int argc, char **argv)
 {
-	struct io_task *mdns_task = NULL, *pcap_task = NULL;
+ 	struct io_task *mdns_task = NULL, *pcap_task = NULL;
 	struct linked_list *iter = NULL;
 
 	if (atexit(on_exit_callback))
