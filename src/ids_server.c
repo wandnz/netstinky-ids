8b75bf34c6c45d020e32ef5e343764e46904b287
Marianne Fletcher <mfletche@astral.cms.waikato.ac.nz>
2019-04-29 11:28:28 +1200

    Print out event list on connection to server.

--- src/ids_server.c
+++ src/ids_server.c
@@ -7,6 +7,133 @@
 
 #include "ids_server.h"
 
+static char *fmt_ioc = "IOC: %s\n";
+static char *fmt_timestamp = "Last seen: %11d.%.9ld\n";
+static char *fmt_event_count = "Number of times seen: %d\n";
+static char *fmt_iface = "Interface: %s\n";
+static char *fmt_src_ip = "Source IP: %s\n\n";
+
+/**
+ * Frees write requests. This should only be called after the request has been
+ * handled (i.e. the write callback called when writing is complete).
+ */
+static void free_write_req(uv_write_t **req)
+{
+	assert(req);
+
+	// TODO: Check how to release uv_write_t properly
+}
+
+void echo_write(uv_write_t *req, int status);
+
+/**
+ * Calculates the length of a string required to hold an IDS event (excluding
+ * termination character).
+ */
+static size_t ids_event_len(struct ids_event *event)
+{
+	struct in_addr ip;
+	char ip_str[30];
+	size_t char_count = 0;
+	int ret;
+
+	char buf[1];
+
+	ret = snprintf(buf, 30, fmt_ioc, event->ioc);
+	if (ret >= 0) char_count += ret;
+
+	ret = snprintf(buf, 30, fmt_timestamp,
+			(long long)event->times_seen->tm_stamp.tv_sec,
+			event->times_seen->tm_stamp.tv_nsec);
+	if (ret >= 0) char_count += ret;
+
+	ret = snprintf(buf, 30, fmt_event_count, event->num_times);
+	if (ret >= 0) char_count += ret;
+
+	ret = snprintf(buf, 30, fmt_iface, event->iface);
+	if (ret >= 0) char_count += ret;
+
+	ret = snprintf(buf, 30, fmt_src_ip, inet_ntop(AF_INET, &ip, ip_str, 20));
+	if (ret >= 0) char_count += ret;
+
+	return char_count;
+}
+
+/**
+ * IOC: <domain name or IP address>
+ * Last seen: <timestamp>
+ * Number of times seen: <unsigned int>
+ * Interface: <iface name>
+ * Source IP: <IP address of infected machine>
+ */
+void write_ids_event(uv_stream_t *stream, struct ids_event *event)
+{
+	// This buffer will be strdup'd so that the contents will outlast this function
+	char *buffer;
+	size_t buf_idx = 0;
+	struct in_addr ip;
+	char ip_str[20];
+	int ret;
+
+	uv_write_t *ioc_req;
+	uv_buf_t ioc_buf;
+
+	size_t buf_sz = ids_event_len(event) + 1;
+	if (NULL == (buffer = malloc(buf_sz)))
+	{
+		fprintf(stderr, "write_ids_event: could not allocate buffer\n");
+		return;
+	}
+
+	ret = snprintf(buffer + buf_idx, buf_sz - buf_idx, fmt_ioc, event->ioc);
+	assert(ret < buf_sz - buf_idx);
+	buf_idx += ret;
+
+	ret = snprintf(buffer + buf_idx, buf_sz - buf_idx, fmt_timestamp,
+			(long long)event->times_seen->tm_stamp.tv_sec,
+			event->times_seen->tm_stamp.tv_nsec);
+	assert(ret < buf_sz - buf_idx);
+	buf_idx += ret;
+
+	ret = snprintf(buffer + buf_idx, buf_sz - buf_idx, fmt_event_count,
+			event->num_times);
+	assert(ret < buf_sz - buf_idx);
+	buf_idx += ret;
+
+	ret = snprintf(buffer + buf_idx, buf_sz - buf_idx, fmt_iface, event->iface);
+	assert(ret < buf_sz - buf_idx);
+	buf_idx += ret;
+
+	ip.s_addr = event->src_ip;
+	// Source IP is stored in network order
+	ret = snprintf(buffer + buf_idx, buf_sz - buf_idx, fmt_src_ip,
+			inet_ntop(AF_INET, &ip, ip_str, 20));
+	assert(ret < buf_sz - buf_idx);
+	buf_idx += ret;
+
+	ioc_buf = uv_buf_init(buffer, buf_sz);
+	if (NULL == (ioc_req = malloc(sizeof(*ioc_req))))
+	{
+		fprintf(stderr, "write_ids_event: could not allocate uv_write_t\n");
+		return;
+	}
+	if (0 > (ret = uv_write(ioc_req, stream, &ioc_buf, 1, echo_write)))
+	{
+		fprintf(stderr, "write_ids_event: write error occurred\n");
+		return;
+	}
+}
+
+void write_ids_event_list(uv_stream_t *stream)
+{
+	struct ids_event *event_iter;
+	struct ids_event_list *list = (struct ids_event_list *)stream->data;
+	for (event_iter = list->head; event_iter; event_iter = event_iter->next)
+	{
+		write_ids_event(stream, event_iter);
+	}
+}
+
 void alloc_buffer(uv_handle_t *handle, size_t suggested_size, uv_buf_t *buf)
 {
 	assert(buf);
@@ -19,7 +146,7 @@ void echo_write(uv_write_t *req, int status)
 {
 	printf("server write\n");
 	if (status) fprintf(stderr, "write error: %s\n", uv_strerror(status));
-	if (req) free(req);
+	if (req) free_write_req(&req);
 }
 
 static void echo_read(uv_stream_t *client, ssize_t nread, const uv_buf_t *buf)
@@ -63,8 +190,15 @@ static void on_new_connection(uv_stream_t *server, int status)
 	if (0 > (err = status)) goto msg;
 	if (NULL == (client = (uv_tcp_t *)malloc(sizeof(*client)))) goto error;
 	if (0 != (err = uv_tcp_init(loop, client))) goto msg;
+
+	// Copy pointer to event list from server to client
+	client->data = server->data;
+
 	if (0 != (err = uv_accept(server, (uv_stream_t *)client))) goto msg;
 	if (0 != (err = uv_read_start((uv_stream_t *)client, alloc_buffer, echo_read))) goto msg;
+
+	write_ids_event_list((uv_stream_t *)client);
+	// Write the current list to the new connection
 	return;
 
 // For error cases where a specific libuv error message can be printed
@@ -75,7 +209,7 @@ error:
 }
 
 int
-setup_event_server(uv_loop_t *loop, uv_tcp_t *handle, int port)
+setup_event_server(uv_loop_t *loop, uv_tcp_t *handle, int port, struct ids_event_list *list)
 {
 	int ret;
 	struct sockaddr_in server_addr;
@@ -85,6 +219,8 @@ setup_event_server(uv_loop_t *loop, uv_tcp_t *handle, int port)
 	assert(port);
 
 	if (0 != uv_tcp_init(loop, handle)) goto error;
+	// Put a pointer to the ids_event_list in the data field of the tcp handle
+	handle->data = list;
 	if (0 != uv_ip4_addr("0.0.0.0", port, &server_addr)) goto error;
 	if (0 != uv_tcp_bind(handle, (const struct sockaddr *)&server_addr, 0)) goto error;
 	ret = uv_listen((uv_stream_t *)handle, 128, on_new_connection);
--- src/ids_server.h
+++ src/ids_server.h
@@ -13,15 +13,19 @@
 
 #include <uv.h>
 
+#include "ids_event_list.h"
+
 /**
  * Sets up libuv variables for the IDS event server.
  *
  * @param loop: The main event loop of the IDS
  * @param handle: The address of an uninitialized uv_tcp_t structure
  * @param server_addr: The interface and port of the server
+ * @param list: The address of the ids_event_list which will be transmitted to
+ * connections.
  * @return 0 if successful, -1 on error
  */
 int
-setup_event_server(uv_loop_t *loop, uv_tcp_t *handle, int port);
+setup_event_server(uv_loop_t *loop, uv_tcp_t *handle, int port, struct ids_event_list *list);
 
 #endif /* SRC_IDS_SERVER_H_ */
