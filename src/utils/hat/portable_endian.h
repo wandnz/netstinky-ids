8b73d36306aa0d6f089e9b2572d3a7cb8cc0711b
Andrew Mackintosh <amackint@waikato.ac.nz>
2020-02-14 14:41:59 +1300

    Configure portable endianness code with autotools

--- src/utils/hat/portable_endian.h
+++ src/utils/hat/portable_endian.h
@@ -1,116 +1,99 @@
-// "License": Public Domain
-// I, Mathias PanzenbÃ¶ck, place this file hereby into the public domain. Use it at your own risk for whatever you like.
-
+/*
+ * portable_endian.h
+ *
+ *  Created on: Jul 18, 2019
+ *      Author: amackint
+ *
+ * Adapted from Public Domain code `portable_endian.h'
+ * written by Mathias Panzenböck to use Autotools
+ */
 #ifndef PORTABLE_ENDIAN_H__
 #define PORTABLE_ENDIAN_H__
 
+#include <config.h>
+
+/* Check include endianness headers discovered by GNU Autotools */
+#if (defined(HAVE_ENDIAN_H))
+#  include <endian.h>
+#elif (defined(HAVE_SYS_ENDIAN_H))
+#  include <sys/endian.h>
+#elif (defined(HAVE_LIBKERN_OSBYTEORDER_H))
+#  include <libkern/OSByteOrder.h>
+#endif
+
+/* Apply platform specific actions */
 #if (defined(_WIN16) || defined(_WIN32) || defined(_WIN64)) && !defined(__WINDOWS__)
-
-# define __WINDOWS__
-
+#  define __WINDOWS__
 #endif
 
 #if defined(__linux__) || defined(__CYGWIN__)
-
-# define __USE_BSD
-# include <endian.h>
-
+#  define __USE_BSD
 #elif defined(__APPLE__)
-
-# include <libkern/OSByteOrder.h>
-
-# define htobe16(x) OSSwapHostToBigInt16(x)
-# define htole16(x) OSSwapHostToLittleInt16(x)
-# define be16toh(x) OSSwapBigToHostInt16(x)
-# define le16toh(x) OSSwapLittleToHostInt16(x)
- 
-# define htobe32(x) OSSwapHostToBigInt32(x)
-# define htole32(x) OSSwapHostToLittleInt32(x)
-# define be32toh(x) OSSwapBigToHostInt32(x)
-# define le32toh(x) OSSwapLittleToHostInt32(x)
- 
-# define htobe64(x) OSSwapHostToBigInt64(x)
-# define htole64(x) OSSwapHostToLittleInt64(x)
-# define be64toh(x) OSSwapBigToHostInt64(x)
-# define le64toh(x) OSSwapLittleToHostInt64(x)
-
-# define __BYTE_ORDER    BYTE_ORDER
-# define __BIG_ENDIAN    BIG_ENDIAN
-# define __LITTLE_ENDIAN LITTLE_ENDIAN
-# define __PDP_ENDIAN    PDP_ENDIAN
-
-#elif defined(__OpenBSD__) || defined(__FreeBSD__)
-
-# include <sys/endian.h>
-
-#elif defined(__NetBSD__) || defined(__DragonFly__)
-
-# include <sys/endian.h>
-
-# define be16toh(x) betoh16(x)
-# define le16toh(x) letoh16(x)
-
-# define be32toh(x) betoh32(x)
-# define le32toh(x) letoh32(x)
-
-# define be64toh(x) betoh64(x)
-# define le64toh(x) letoh64(x)
-
+#  define __BYTE_ORDER    BYTE_ORDER
+#  define __BIG_ENDIAN    BIG_ENDIAN
+#  define __LITTLE_ENDIAN LITTLE_ENDIAN
+#  define __PDP_ENDIAN    PDP_ENDIAN
 #elif defined(__WINDOWS__)
-
-# include <winsock2.h>
-# include <sys/param.h>
-
-# if BYTE_ORDER == LITTLE_ENDIAN
-
-#   define htobe16(x) htons(x)
-#   define htole16(x) (x)
-#   define be16toh(x) ntohs(x)
-#   define le16toh(x) (x)
- 
-#   define htobe32(x) htonl(x)
-#   define htole32(x) (x)
-#   define be32toh(x) ntohl(x)
-#   define le32toh(x) (x)
- 
-#   define htobe64(x) htonll(x)
-#   define htole64(x) (x)
-#   define be64toh(x) ntohll(x)
-#   define le64toh(x) (x)
-
-# elif BYTE_ORDER == BIG_ENDIAN
-
-    /* that would be xbox 360 */
-#   define htobe16(x) (x)
-#   define htole16(x) __builtin_bswap16(x)
-#   define be16toh(x) (x)
-#   define le16toh(x) __builtin_bswap16(x)
- 
-#   define htobe32(x) (x)
-#   define htole32(x) __builtin_bswap32(x)
-#   define be32toh(x) (x)
-#   define le32toh(x) __builtin_bswap32(x)
- 
-#   define htobe64(x) (x)
-#   define htole64(x) __builtin_bswap64(x)
-#   define be64toh(x) (x)
-#   define le64toh(x) __builtin_bswap64(x)
-
-# else
-
-#   error byte order not supported
-
-# endif
-
-# define __BYTE_ORDER    BYTE_ORDER
-# define __BIG_ENDIAN    BIG_ENDIAN
-# define __LITTLE_ENDIAN LITTLE_ENDIAN
-# define __PDP_ENDIAN    PDP_ENDIAN
-
+#  include <winsock2.h>
+#  include <sys/param.h>
+#  define __BYTE_ORDER    BYTE_ORDER
+#  define __BIG_ENDIAN    BIG_ENDIAN
+#  define __LITTLE_ENDIAN LITTLE_ENDIAN
+#  define __PDP_ENDIAN    PDP_ENDIAN
 #else
-
-# error platform not supported
-
+#  error platform not supported
 #endif
 
+#ifndef htobe32
+#  if HAVE_DECL_OSSWAPHOSTTOBIGINT32
+#    define htobe32(x) OSSwapHostToBigInt32(x)
+#  elif ENDIANNESS == LITTLE_ENDIAN
+#    define htobe32(x) htonl(x)
+#  elif ENDIANNESS == BIG_ENDIAN
+#    define htobe32(x) (x)
+#  else
+#    error byte order not supported
+#  endif
 #endif
+
+#ifndef be32toh
+#  if HAVE_DECL_OSSWAPBIGTOHOSTINT32
+#    define be32toh(x) OSSwapBigToHostInt32(x)
+#  elif defined(betoh32)
+#    define be32toh(x) betoh32(x)
+#  elif ENDIANNESS == LITTLE_ENDIAN
+#    define be32toh(x) ntohl(x)
+#  elif ENDIANNESS == BIG_ENDIAN
+#    define be32toh(x) (x)
+#  else
+#    error byte order not supported
+#  endif
+#endif
+
+#ifndef htobe64
+#  if HAVE_DECL_OSSWAPHOSTTOBIGINT64
+#    define htobe64(x) OSSwapHostToBigInt64(x)
+#  elif ENDIANNESS == LITTLE_ENDIAN
+#    define htobe64(x) htonll(x)
+#  elif ENDIANNESS == BIG_ENDIAN
+#    define htobe64(x) (x)
+#  else
+#    error byte order not supported
+#  endif
+#endif
+
+#ifndef be64toh
+#  ifdef HAVE_DECL_OSSWAPBIGTOHOSTINT64
+#    define be64toh(x) OSSwapBigToHostInt64(x)
+#  elif defined(betoh64)
+#    define be64toh(x) betoh64(x)
+#  elif ENDIANNESS == LITTLE_ENDIAN
+#    define be64toh(x) ntohll(x)
+#  elif ENDIANNESS == BIG_ENDIAN
+#    define be64toh(x) (x)
+#  else
+#    error byte order not supported
+#  endif
+#endif
+
+#endif /* PORTABLE_ENDIAN_H__ */
